53342b63c82b70dc517980cc511137926382f85b
Varshini618 <110109639+Varshini618@users.noreply.github.com>
2022-07-27 20:27:29 +0530

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -19,5 +19,14 @@ int main()
              "\nChoice: ");
       scanf("%d", &choice);
    }
+   switch (choice)
+   {
+   case 1:
+      printf("Enter two numbers: \n");
+      scanf("%ld %ld", &num1, &num2);
+      x = num1 + num2;
+      printf("Sum = %ld", x);
+      break;
    return 0;
+   }
 }
